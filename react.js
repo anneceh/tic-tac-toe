e30735f4dd5b721f7ce0b27708c2065aef51ca0d
Cecilie Haugstvedt <anneceh@users.noreply.github.com>
2017-04-05 22:11:41 +0200

    Finished games with list of moves

--- react.js
+++ react.js
@@ -43,7 +43,14 @@ class Game extends React.Component {
         squares: Array(9).fill(null)
       }],
       xIsNext: true,
+      stepNumber: 0,
     };
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ? false : true,
+    });
   }
   handleClick(i) {
     const history = this.state.history;
@@ -56,26 +63,34 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{squares: squares}]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
   }
   render() {
     const history = this.state.history;
-    const current = history[history.length-1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Move #' + move : 'Game start';
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>);
+    });
     return (
       <div className="game">
         <div className="game-board">
           <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
